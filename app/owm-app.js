63428dd064a02ebd2d82a1beda51379fa301d2e5
bonnichiwa <bonnie.pham@ryerson.ca>
2015-09-24 13:51:13 -0400

    added links to index, and template displaying error if cities cannot be found in owmCities values

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,4 +1,6 @@
 angular.module('OWMApp', ['ngRoute'])
+  .value('owmCities', ['New York', 'Dallas', 'Chicago'])
+
   .config(['$routeProvider', function($routeProvider){
     $routeProvider.when('/', {
         templateUrl : 'home.html',
@@ -6,12 +8,25 @@ angular.module('OWMApp', ['ngRoute'])
       })
       .when('/cities/:city', {
         templateUrl : 'city.html',
-        controller : 'CityCtrl'
-      });
+        controller : 'CityCtrl',
+        resolve : {
+          city : function(owmCities, $route, $location) {
+            var city = $route.current.params.city;
+            if(owmCities.indexOf(city) == -1) {
+              $location.path('/error');
+                return;
+              }
+              return city;
+          }
+        }
+      })
+      .when('/error', {
+        template : '<p>Error - Page Not Found</p>'
+      })
   }])
   .controller('HomeCtrl', ['$scope', function($scope) {
     //empty for now
   }])
-  .controller('CityCtrl', ['$scope', '$routeParams', function($scope, $routeParams) {
-    $scope.city = $routeParams.city;
-  }]);
+  .controller('CityCtrl', function($scope, city) {
+    $scope.city = city;
+  });
