1b20b5c2c77e98764d9c113718f1999b5f99a459
bonnichiwa <bonnie.pham@ryerson.ca>
2015-09-24 12:48:28 -0400

    added home page route

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,1 +1,10 @@
-angular.module('OWMApp', ['ngRoute']);
+angular.module('OWMApp', ['ngRoute'])
+  .config(['$routeProvider', function($routeProvider){
+      $routeProvider.when('/', {
+          templateUrl : 'home.html',
+          controller : 'HomeCtrl'
+      });
+  }])
+  .controller('HomeCtrl', function($scope) {
+      //empty for now
+  });;
