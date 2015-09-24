344efac6b0ab98b8790654599a01c6bbfec30a31
bonnichiwa <bonnie.pham@ryerson.ca>
2015-09-24 13:13:37 -0400

    added city page, with hardcoded in city

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,10 +1,16 @@
 angular.module('OWMApp', ['ngRoute'])
   .config(['$routeProvider', function($routeProvider){
-      $routeProvider.when('/', {
-          templateUrl : 'home.html',
-          controller : 'HomeCtrl'
+    $routeProvider.when('/', {
+        templateUrl : 'home.html',
+        controller : 'HomeCtrl'
+      }).when('/city', {
+        templateUrl : 'city.html',
+        controller : 'CityCtrl'
       });
   }])
   .controller('HomeCtrl', function($scope) {
-      //empty for now
-  });;
+    //empty for now
+  })
+  .controller('CityCtrl', function($scope) {
+    $scope.city = 'New York';
+  });
