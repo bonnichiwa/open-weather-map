adba35ff504c089f9eea0cf15bebb1f2628cefad
bonnichiwa <bonnie.pham@ryerson.ca>
2015-09-24 21:21:19 -0400

    added loading gif in between routes redirecting

--- app/owm-app.js
+++ app/owm-app.js
@@ -1,8 +1,16 @@
 angular.module('OWMApp', ['ngRoute'])
 
-  .run(function($rootScope, $location) {
+  .run(function($rootScope, $location, $timeout) {
     $rootScope.$on('$routeChangeError', function() {
       $location.path('/error');
+    });
+    $rootScope.$on('$routeChangeStart', function() {
+      $rootScope.isLoading = true;
+    });
+    $rootScope.$on('$routeChangeSuccess', function() {
+      $timeout(function() {
+        $rootScope.isLoading = false;
+      }, 1000);
     });
   })
 
